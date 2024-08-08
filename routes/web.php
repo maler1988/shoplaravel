8e0cd35b766923084efbcc2d3bf5febf1fda01e6
Alexandr <maler1988@yandex.ru>
2024-08-08 17:19:38 +0300

    add models, controllers, seeders, factories + views

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ShopController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::resource('/products', ProductController::class);
+Route::resource('/shops', ShopController::class);
